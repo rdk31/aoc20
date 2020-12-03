6b88c0426fe8fd0e130dce10d897c908a311ef62
rdk31 <rdk31@protonmail.com>
2020-12-03 14:22:01 +0100

    fix day2

--- 2020/day2/src/main.rs
+++ 2020/day2/src/main.rs
@@ -14,11 +14,11 @@ fn main() -> io::Result<()> {
         let first_split: Vec<&str> = b.split(":").collect();
         let password = &first_split[1][1..];
         let second_split: Vec<&str> = first_split[0].split(" ").collect();
-        let character = second_split[1];
+        let character = second_split[1].chars().nth(0).unwrap();
         let third_split: Vec<&str> = second_split[0].split("-").collect();
 
-        let lower_bounds: i32 = third_split[0].parse().unwrap();
-        let higher_bounds: i32 = third_split[1].parse().unwrap();
+        let lower_bounds: i32 = third_split[0].parse::<i32>().unwrap();
+        let higher_bounds: i32 = third_split[1].parse::<i32>().unwrap();
 
         let count = password.matches(character).count() as i32;
 
@@ -26,10 +26,11 @@ fn main() -> io::Result<()> {
             part1 += 1;
         }
 
-        if (password.as_bytes()[(lower_bounds - 1) as usize] == character.as_bytes()[0]
-            && password.as_bytes()[(higher_bounds - 1) as usize] != character.as_bytes()[0])
-            || (password.as_bytes()[(lower_bounds - 1) as usize] != character.as_bytes()[0]
-                && password.as_bytes()[(higher_bounds - 1) as usize] == character.as_bytes()[0])
+        let first_char = password.chars().nth((lower_bounds - 1) as usize).unwrap();
+        let second_char = password.chars().nth((higher_bounds - 1) as usize).unwrap();
+
+        if (first_char == character && second_char != character)
+            || (first_char != character && second_char == character)
         {
             part2 += 1;
         }
