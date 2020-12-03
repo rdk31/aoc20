caf84b8a7a93fc83be53bb18640cfd9ce62bedcd
rdk31 <rdk31@protonmail.com>
2020-12-03 17:27:22 +0100

    day3 fix naming

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-fn get_scopes(map: &Vec<Vec<char>>, x_offset: usize, y_offset: usize) -> i64 {
+fn get_hit_trees(map: &Vec<Vec<char>>, slope_x: usize, slope_y: usize) -> i64 {
     let width = map[0].len();
 
     let mut x = 0;
@@ -13,8 +13,8 @@ fn get_scopes(map: &Vec<Vec<char>>, x_offset: usize, y_offset: usize) -> i64 {
             trees += 1;
         }
 
-        x += x_offset;
-        y += y_offset;
+        x += slope_x;
+        y += slope_y;
     }
 
     trees
@@ -31,14 +31,14 @@ fn main() -> io::Result<()> {
 
     // println!("{:?}", map);
 
-    let part1 = get_scopes(&map, 3, 1);
+    let part1 = get_hit_trees(&map, 3, 1);
     println!("part1: {}", part1);
 
-    let mut part2 = get_scopes(&map, 1, 1);
+    let mut part2 = get_hit_trees(&map, 1, 1);
     part2 *= part1;
-    part2 *= get_scopes(&map, 5, 1);
-    part2 *= get_scopes(&map, 7, 1);
-    part2 *= get_scopes(&map, 1, 2);
+    part2 *= get_hit_trees(&map, 5, 1);
+    part2 *= get_hit_trees(&map, 7, 1);
+    part2 *= get_hit_trees(&map, 1, 2);
     println!("part2: {}", part2);
 
     Ok(())
